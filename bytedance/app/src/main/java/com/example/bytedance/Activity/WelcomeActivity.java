fbc438a0b317f15202c0e7c51c6b1b7deeed2e07
zzc <916729656@qq.com>
2020-12-01 10:54:38 +0800

    完善广告页功能

--- bytedance/app/src/main/java/com/example/bytedance/Activity/WelcomeActivity.java
+++ bytedance/app/src/main/java/com/example/bytedance/Activity/WelcomeActivity.java
@@ -5,26 +5,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
-
 import com.example.bytedance.R;
-/**
- *
- * 1.声明界面
- * 2.定义变量
- * 3.调用类Animation
- * 4.写方法让它动起来
- * @author Rain
- *
- */
+
 public class WelcomeActivity extends Activity{
     // 声明控件对象
     private TextView textView;
+    private Button btnView;
     //声明时间有多少;
     private int count = 5;
     private Animation animation;
@@ -35,9 +29,10 @@ public class WelcomeActivity extends Activity{
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         // 初始化控件对象textView
-        textView = (TextView) findViewById(R.id.textView);
+        //textView = (TextView) findViewById(R.id.textView);
+        btnView = (Button) findViewById(R.id.btn_splash);
         animation = AnimationUtils.loadAnimation(this, R.anim.animation_text);
-        handler.sendEmptyMessageDelayed(0, 1000);
+        mHandler.sendEmptyMessageDelayed(0, 1000);
     }
     //咱在写一个计算Welcome界面的广告时间结束后进入主界面的方法
     private int getCount() {
@@ -49,21 +44,21 @@ public class WelcomeActivity extends Activity{
         }
         return count;
     }
+
+
     //进行一个消息的处理
     @SuppressLint("HandlerLeak")
-    private Handler handler = new Handler() {
-        public void handleMessage(android.os.Message msg) {
+    private Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(android.os.Message msg) {
             if (msg.what == 0) {
-                textView.setText(getCount()+"");
-                handler.sendEmptyMessageDelayed(0, 1000);
+                //textView.setText(getCount()+"");
+                btnView.setText("倒计时："+ getCount()+"s");
+                mHandler.sendEmptyMessageDelayed(0, 1000);
                 animation.reset();
-                textView.startAnimation(animation);
+                //textView.startAnimation(animation);
             }
-        };
-    };
+            return false;
+        }
+    });
 }
-
-
-
-
-
